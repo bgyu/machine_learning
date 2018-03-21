f951065021659a5f6bc258b5cfaf7d0350c01fc7
Ben Yu <cnhnyu@gmail.com>
2018-03-21 15:48:43 +1100

    Finish Cost function and random weights

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,28 +62,63 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 % -------------------------------------------------------------
+
 % Part 1
-a1 = [ones(m, 1), X];  % size(a1) = 5000 * 401
+a1 = [ones(m, 1) X];
 
-z2 = a1 * Theta1';  % size(Theta1) = 25 * 401, size(z2 = 5000 * 25)
-a2 = sigmoid(z2); % size(a2) = 5000 * 25
-a2 = [ones(size(a2, 1), 1) a2];  % size(a2) = 5000 * 26
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2,1), 1) a2];
 
-z3 = a2 * Theta2'; % size(a2) = 5000 * 26, size(Theta2) = 10 * 26, size(z3) = 5000 * 10
+z3 = a2 * Theta2';
 a3 = sigmoid(z3);
-hThetaX = a3;  % size(hThetaX) = size(a3) = 50000 * 10
+hThetaX = a3;
 
-term1 = -y .* log(hThetaX);
-term2 = (1-y) .* log(1 - hThetaX);
+yVec = zeros(m, num_labels);
 
-J = (1/m) * sum(term1 - term2);
+for i = 1:m
+    yVec(i, y(i)) = 1;
+end
 
-regulator = lambda / ( 2 * m) * (sum(sum(Theta1(:, 2:end).^2)) + sum(sum(Theta2(:, 2:end).^2)));
+J = 1/m * sum(sum(-1 * yVec .* log(hThetaX)-(1-yVec) .* log(1-hThetaX)));
 
-J = J + regulator;
-% Part 2
+regularator = (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2))) * (lambda/(2*m));
 
-% Part 3
+J = J + regularator;
+
+%% Part 2 implementation
+
+
+
+for t = 1:m
+
+	% For the input layer, where l=1:
+	a1 = [1; X(t,:)'];
+
+	% For the hidden layers, where l=2:
+	z2 = Theta1 * a1;
+	a2 = [1; sigmoid(z2)];
+
+	z3 = Theta2 * a2;
+	a3 = sigmoid(z3);
+
+	yy = ([1:num_labels]==y(t))';
+	% For the delta values:
+	delta_3 = a3 - yy;
+
+	delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
+	delta_2 = delta_2(2:end); % Taking of the bias row
+
+	% delta_1 is not calculated because we do not associate error with the input    
+
+	% Big delta update
+	Theta1_grad = Theta1_grad + delta_2 * a1';
+	Theta2_grad = Theta2_grad + delta_3 * a2';
+end
+
+Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
+
 
 % =========================================================================
 
